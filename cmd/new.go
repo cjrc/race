0b0d78f3901be8e0adec79c59f27a173d09e52d1
Greg Hull <greg.hull@gmail.com>
2019-02-13 10:52:16 -0500

    add sql code to create schema

--- cmd/new.go
+++ cmd/new.go
@@ -8,39 +8,62 @@ import (
 	"io"
 	"os"
 
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq" // database driver for Postgres
 	"github.com/spf13/cobra"
 )
 
 var dbConn string
 
-const createEntriesTableSQL = `
-	CREATE TABLE IF NOT EXISTS Entries (
-		id INTEGER PRIMARY KEY
-	);`
-
-const createResultsTableSQL = `
-	CREATE TABLE IF NOT EXISTS Results (
-		id INTEGER PRIMARY KEY
-		place INTEGER DEFAULT 0
-		time INTEGER DEFAULT 0
-		avg_pace INTEGER DEFAULT 0
-		distance INTEGER DEFAULT 0
-		name VARCHAR(80) DEFAULT ""
-		bib_num INTEGER UNIQUE
-		class VARCHAR(20) DEFAULT ""
-	);`
-
-const createEventsTableSQL = `
-CREATE TABLE IF NOT EXISTS Events (
-	id INTEGER PRIMARY KEY
-
-);`
-
-const createRacesTableSQL = `
-CREATE TABLE IF NOT EXISTS Races (
-	id INTEGER PRIMARY KEY
-
-);`
+// SQLcommands are for creating the data tables, indices, etc
+var schema = []string{
+	`CREATE TABLE Entries (
+		id SERIAL PRIMARY KEY,
+		email TEXT DEFAULT '',
+		club_name TEXT DEFAULT '',
+		club_abbrev TEXT DEFAULT '',
+		seed TEXT DEFAULT '0:00',
+		age INTEGER DEFAULT 0,
+		boat_name TEXT DEFAULT ' ',
+		country TEXT DEFAULT 'USA', 
+		event_id INTEGER DEFAULT 0,
+		race_id INTEGER DEFAULT 0,
+		lane INTEGER DEFAULT 0,
+		scratched BOOLEAN DEFAULT false,
+		ltwt BOOLEAN DEFAULT false
+	);`,
+	"CREATE INDEX ON Entries (race_id);",
+	"CREATE INDEX ON Entries (event_id);",
+	`CREATE TABLE Results (
+		id SERIAL PRIMARY KEY,
+		place INTEGER DEFAULT 0,
+		time INTEGER DEFAULT 0,
+		avg_pace INTEGER DEFAULT 0,
+		distance INTEGER DEFAULT 0,
+		name text DEFAULT ''::text,
+		entry_id INTEGER UNIQUE,
+		class VARCHAR(20) DEFAULT ''::text
+	);`,
+	"CREATE INDEX ON Results (entry_id);",
+	`CREATE TABLE Events (
+		id SERIAL PRIMARY KEY,
+		name TEXT DEFAULT ''::text,
+		start TEXT DEFAULT '8:00AM'::text,
+		distance INTEGER DEFAULT 2000,
+		official bool DEFAULT false
+	);`,
+	`CREATE TABLE Races (
+		id SERIAL PRIMARY KEY,
+		boat_type INTEGER DEFAULT 0,
+		name TEXT DEFAULT ''::text,
+		distance INTEGER DEFAULT 2000,
+		enable_stroke_data BOOLEAN DEFAULT false,
+		split_distance INTEGER DEFAULT 500,
+		split_times INTEGER DEFAULT 120,
+		nlanes INTEGER DEFAULT 10,
+		duration_type INTEGER DEFAULT 0
+	);`,
+}
 
 // initCmd represents the init command
 var newCmd = &cobra.Command{
@@ -82,6 +105,12 @@ to quickly create a Cobra application.`,
 			os.Exit(1)
 		}
 
+		fmt.Println("creating database schema")
+		if err := createDatabase(); err != nil {
+			fmt.Println("Cannot create database tables:", err)
+			os.Exit(1)
+		}
+
 		// Create the new regatta
 		fmt.Println("creating default config")
 		C.WriteToFile("race.yaml")
@@ -115,6 +144,21 @@ func isDirEmpty(name string) (bool, error) {
 		return true, nil
 	}
 	return false, err // Either not empty or error, suits both cases
+}
+
+func createDatabase() error {
+	db, err := sqlx.Connect("postgres", C.DB)
+	if err != nil {
+		return err
+	}
+
+	for _, s := range schema {
+		if _, err := db.Exec(s); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func init() {

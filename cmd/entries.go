671a18d90984e2c043e395f5a0c16437ec4e2547
Greg Hull <greg@cjrc.us>
2019-02-14 08:52:55 -0500

    break import entries command into a separate source file

--- cmd/entries.go
+++ cmd/entries.go
@@ -1,26 +1,27 @@
-// Copyright © 2019 CJRC, Inc <greg@jrc.us>
-//
-
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
 // importCmd represents the import command
-var importCmd = &cobra.Command{
-	Use:   "import",
-	Short: "Import entries or results",
+var entriesCmd = &cobra.Command{
+	Use:   "entries",
+	Short: "Import race entries from RegattaCentral",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("import called")
+	},
 }
 
 func init() {
-	rootCmd.AddCommand(importCmd)
 	importCmd.AddCommand(entriesCmd)
 
 	// Here you will define your flags and configuration settings.

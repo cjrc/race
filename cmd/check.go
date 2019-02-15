bbed0649ad672d02af3b99c97db1fe26a335f5b4
Greg Hull <greg@cjrc.us>
2019-02-15 10:44:04 -0500

    added the check command

---
+++ cmd/check.go
@@ -0,0 +1,56 @@
+// Copyright © 2019 CJRC, Inc <greg@jrc.us>
+//
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// checkCmd represents the check command
+var checkCmd = &cobra.Command{
+	Use:   "check",
+	Short: "Run various sanity checks",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+}
+
+var checkDatabaseCmd = &cobra.Command{
+	Use:   "database",
+	Short: "Check the database connection",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if _, err := DBConnect(); err != nil {
+			fmt.Println("Cannot connect to database:", err)
+		} else {
+			fmt.Println("Database connection is good!")
+		}
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(checkCmd)
+	checkCmd.AddCommand(checkDatabaseCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// checkCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// checkCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
